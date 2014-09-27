95b5a4c68c351fff09533bffbee15728cb51b226
Chris Gibb <chrisrgibb@gmail.com>
2014-09-28 08:58:05 +1300

    awertlwker

--- level/LevelGenerator.js
+++ level/LevelGenerator.js
@@ -20,7 +20,14 @@
 var levelGenerator = function(){
 	var height = 12;
 	var levelSettings = LevelSettings();
-	var groundTile = levelSettings.groundTile();
+	var groundTile,
+		hazard1,
+		hazard2,
+		breakable,
+		unbreakable;
+	var groundIndex = 10;
+
+
 	this.groundTile = 18;
 	this.obsticleTile1 = 24;
 	this.obsticleTile2 = 23;
@@ -36,9 +43,30 @@ var levelGenerator = function(){
 	this.enemies = [];
 	this.objects  = [];
 
+	init();
+
+	function init(){
+		console.log("init");
+		setUpLevel('one');
 
 
+	}
+
+	function setUpLevel(theme){
+		levelSettings.setTheme(theme);
+		groundTile = levelSettings.groundTile();
+		hazard1 = levelSettings.hazard1();
+		hazard2 = levelSettings.hazard2();
+		breakable = levelSettings.breakable();
+		unbreakable = levelSettings.unbreakable();
+	}
+
+
+	/*
+	*	array of plain tiles with two tiles on the ground
+	*/
 	function createPlainLevel(length){
+		
 		var tiles = new Array(height);
 		for(var i =0; i< tiles.length; i++){
 			tiles[i] = [];
@@ -54,10 +82,115 @@ var levelGenerator = function(){
 		return tiles;
 	}
 
-		function createlevel(){
-			var tiles = createPlainLevel(100);
-			return tiles;
+	var randomInt = function(size){
+		return Math.round(Math.random() * size);
+	}
+
+	function createlevel(){
+		setUpLevel("two");
+		// get theme
+		var length = 100;
+
+		var tiles = createPlainLevel(length);
+		var odds = [];
+		odds[0] = 30; // "createGap"
+		odds[1] = 30; //"makePlatform"
+		odds[2] = 5;  //"triangleThing"
+		odds[3] = 30; // "flatGround"
+		var totalOdds = 0;
+
+		for(var i = 0 ; i < odds.length; i++){
+
+			if(odds[i] < 0 ){
+				odds[i]= 0;
+			}
+			totalOdds+=odds[i];
+			odds[i] = totalOdds - odds[i];
 		}
+		var index = 12;
+		while (index < length) {
+			var areaSize = 3 + Math.random() * 12 | 0;
+			var chance = Math.random() * totalOdds;
+			var type;
+			for (var i = 0; i< odds.length; i++) {
+				if (odds[i] <= chance) {
+					type = i;
+				}
+			}
+			switch (type) {
+				case 0:
+					createOneGap(tiles, index, areaSize-2);
+					break;				
+				case 1:
+					createOneGap(tiles, index, areaSize-2);
+					// this.makePlatform(index, size-2);
+					break;
+				case 2:
+					// createOneGap(tiles, index, areaSize-2);
+					triangleThing(tiles, index, areaSize-2);
+					// this.triangleThing(5);
+					break;
+				case 3 :
+					createOneGap(tiles, index, areaSize-2);
+					// this.plainSquare(index, size-2);
+					// this.straightVertical(index, size-2);
+					break;
+			}
+			// this.addClouds(index, 6);
+			index+=areaSize;
+		}
+
+		return tiles;
+	}
+
+
+	function createOneGap(tiles, index, length){
+		// so we don't create a gap near the end of the level
+		if(length > 5){
+			var randHeight = height - randomInt(2) - 2;
+			var randX = randomInt(length)+ (index - 2);
+			tiles[randHeight][randX] = breakable;
+		}
+
+
+		for(var i = index+1; i< index + length-1; i++){
+			if(i > tiles[0].length - 14){
+				// debugger;
+				return;
+			}
+			tiles[height-2][i] = hazard1;
+			tiles[height-1][i] = hazard2;
+		}
+	}
+
+	function triangleThing(tiles, index, length){
+		/* options 
+		   plain triangle
+		   triangle with random money
+		   triangle with random unbreakable bits
+		*/
+		debugger;
+		// width of triangle will  be 2n -1 wide
+		var height = Math.ceil(length / 2);
+	
+
+		var startX = index;
+		var middle = startX + (height - 1);
+
+		for(var i = 0; i< height; i++){
+			// start at top
+			// for triangles with 2 blocks at top initialize j to startX + (height - i)
+
+			for(var j = startX + (height - i -1); j < startX + (height + i ); j++ ){  
+				tiles[groundIndex - height  + i][j] = breakable; 
+				
+				if(Math.random() > .4 && j==middle){
+					tiles[groundIndex - height  + i][j] = unbreakable; 
+				}
+			}
+		}
+	}
+
 
 	return {
 		createNewMap: function(){
@@ -74,190 +207,9 @@ var levelGenerator = function(){
 			return map;
 		}
 	};
-
-
-
-	// return {
-	// 	createlevel: function(){
-	// 		// Choose theme
-	// 		var possibleOptions = ["one", "two", "three"];
-	// 		var poss = Math.random() * 3 | 0;
-	// 		var odds = [];
-	// 		// this.currentOption = possibleOptions[poss];
-	// 		this.currentOption = ["one"];
-			
-	// 		odds[0] = 30; // "createGap"
-	// 		odds[1] = 30; //"makePlatform"
-	// 		odds[2] = 5;  //"triangleThing"
-	// 		odds[3] = 30; // "flatGround"
-	// 		var totalOdds = 0;
-
-	// 		for(var i = 0 ; i < odds.length; i++){
-
-	// 			if(odds[i] < 0 ){
-	// 				odds[i]= 0;
-	// 			}
-	// 			totalOdds+=odds[i];
-	// 			odds[i] = totalOdds - odds[i];
-	// 		}
-
-	// 		// map length
-	// 		var length = 32 + (Math.random() * 128) | 0;
-	// 		this.EndingX = length - randomInt(3) - 1; // area to put the rice ball / hamburger
-
-	// 		this.tiles = new Array(this.height);
-	// 		this.createPlainlevel(this.tiles, length);
-
-	// 		var start = index = 12;
-	// 		while(index < length){
-
-	// 			var size = 3 + Math.random() * 12 | 0;
-
-	// 			this.enemies.push(new Enemy(index, randomInt(5)  ));
-	// 			var chance = Math.random() * totalOdds;
-	// 			var type;
-	// 			for (var i = 0; i< odds.length; i++) {
-	// 				if (odds[i] <= chance) {
-	// 					type = i;
-	// 				}
-	// 			}
-	// 			this.plainSquare(index, size-4);
-	// 			// switch (type) {
-	// 			// 	case 0:
-	// 			// 		this.createOneGap(index, size-2);
-	// 			// 		break;				
-	// 			// 	case 1:
-	// 			// 		this.makePlatform(index, size-2);
-	// 			// 		break;
-	// 			// 	case 2:
-	// 			// 		this.triangleThing(5);
-	// 			// 		break;
-	// 			// 	case 3 :
-	// 			// 		this.plainSquare(index, size-2);
-	// 			// 		// this.straightVertical(index, size-2);
-	// 			// 		break;
-	// 			// }
-
-	// 			this.addClouds(index, 6);
-
-	// 			index+=size; 
-	// 		}
-	// 		return this.tiles;
-	// 	}
-
-
-	// };
-}
-
-CONSTANTS.themeOptions = {
-	"one" : { 
-		"groundTile" : 18,
-		"hazard1" : 23,
-		"hazard2" : 22,
-		"breakable" : 11,
-		"unbreakable" : 12
-	},
-	"two" : {
-		"groundTile" : 13,
-		"hazard1" : 28,
-		"hazard2" : 27,
-		"breakable" : 21,
-		"unbreakable" : 20
-	},
-	"three" : {
-		"groundTile" : 32,
-		"hazard1" : 33,
-		"hazard2" : 33,
-		"breakable" : 35,
-		"unbreakable" : 32
-	}
 }
 
 
-levelGenerator.prototype.createNewMap = function(random, difficulty){
-	var map;
-	if(random){
-		var tiles = this.createlevel();
-		map = new Map(tiles);
-	}else {
-		map = new Map();
-	}
-
-	map.enemys = this.enemies;
-	map.objects.push(new RiceBall(this.EndingX, 8));
-	return map;
-}
-
-
-
-
-
-levelGenerator.prototype.createlevel = function() {
-	// Choose theme
-	var possibleOptions = ["one", "two", "three"];
-	var poss = Math.random() * 3 | 0;
-	var odds = [];
-	// this.currentOption = possibleOptions[poss];
-	this.currentOption = ["one"];
-	
-	odds[0] = 30; // "createGap"
-	odds[1] = 30; //"makePlatform"
-	odds[2] = 5;  //"triangleThing"
-	odds[3] = 30; // "flatGround"
-	var totalOdds = 0;
-
-	for(var i = 0 ; i < odds.length; i++){
-
-		if(odds[i] < 0 ){
-			odds[i]= 0;
-		}
-		totalOdds+=odds[i];
-		odds[i] = totalOdds - odds[i];
-	}
-
-	// map length
-	var length = 32 + (Math.random() * 128) | 0;
-	this.EndingX = length - randomInt(3) - 1; // area to put the rice ball / hamburger
-
-	this.tiles = new Array(this.height);
-	this.createPlainlevel(this.tiles, length);
-
-	var start = index = 12;
-	while(index < length){
-
-		var size = 3 + Math.random() * 12 | 0;
-
-		this.enemies.push(new Enemy(index, randomInt(5)  ));
-		var chance = Math.random() * totalOdds;
-		var type;
-		for (var i = 0; i< odds.length; i++) {
-			if (odds[i] <= chance) {
-				type = i;
-			}
-		}
-		this.plainSquare(index, size-4);
-		// switch (type) {
-		// 	case 0:
-		// 		this.createOneGap(index, size-2);
-		// 		break;				
-		// 	case 1:
-		// 		this.makePlatform(index, size-2);
-		// 		break;
-		// 	case 2:
-		// 		this.triangleThing(5);
-		// 		break;
-		// 	case 3 :
-		// 		this.plainSquare(index, size-2);
-		// 		// this.straightVertical(index, size-2);
-		// 		break;
-		// }
-
-		this.addClouds(index, 6);
-
-		index+=size; 
-	}
-	return this.tiles;
-};
 
 
 
@@ -384,25 +336,6 @@ function get2dArray(size){
 	return tiles;
 }
 
-levelGenerator.prototype.createOneGap = function(startX, length){
-	var themeOptions = CONSTANTS.themeOptions;
-	if(length > 5){
-		this.tiles[this.height - 4][startX + 4] = themeOptions[this.currentOption].unbreakable;
-		this.tiles[this.height - randomInt(2) - 4 ][startX + randomInt(8) ] = themeOptions[this.currentOption].breakable;
-	}
-
-	for(var i = startX+1; i< startX+ length-1; i++){
-		this.tiles[this.height-2][i] = themeOptions[this.currentOption].hazard1;
-		this.tiles[this.height-1][i] = themeOptions[this.currentOption].hazard2;
-	}
-	var ranran = Math.random();
-	if(ranran > .33 && ranran < .45 ){
-		// create bridge over it
-
-
-	}
-}
-
 
 levelGenerator.prototype.addClouds = function(startX, size){
 	// var numberOfClouds = randomInt(10)+1;
@@ -463,9 +396,7 @@ levelGenerator.prototype.triangleThing = function(length){
 }
 
 
-var randomInt = function(size){
-	return Math.round(Math.random() * size);
-}
+
 
 
 
